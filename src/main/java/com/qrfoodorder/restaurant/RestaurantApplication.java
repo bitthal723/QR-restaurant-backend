36da495e398a94248c783fb53aacd0242cf6c4cf
bitthalChhipa723 <175926556+bitthal723@users.noreply.github.com>
2025-04-27 23:09:28 +0530

    removed serviceAccountKey.json

--- src/main/java/com/qrfoodorder/restaurant/RestaurantApplication.java
+++ src/main/java/com/qrfoodorder/restaurant/RestaurantApplication.java
@@ -1,24 +1,38 @@
 package com.qrfoodorder.restaurant;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @SpringBootApplication
 public class RestaurantApplication {
+	public static Firestore firestore;
+
 	public static void main(String[] args) throws IOException {
-		FileInputStream serviceAccount =
-				new FileInputStream("src/main/resources/serviceAccountKey.json");
+//		FileInputStream serviceAccount =
+//				new FileInputStream("src/main/resources/serviceAccountKey.json");
+//
+//		FirebaseOptions options = FirebaseOptions.builder()
+//				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//				.build();
+//
+//		FirebaseApp.initializeApp(options);
 
-		FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build();
 
-		FirebaseApp.initializeApp(options);
+		try {
+			FirestoreOptions firestoreOptions =
+					FirestoreOptions.getDefaultInstance().toBuilder().build();
+			firestore = firestoreOptions.getService();
+			System.out.println("Firestore initialized successfully!");
+		} catch (Exception e) {
+			System.out.println("Failed to initialize Firestore!");
+			e.printStackTrace();
+		}
+
+
+
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
 }
